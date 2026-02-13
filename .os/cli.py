3829e9affc755a4094b62359fe633f5a49bfbdae
DeLulu <smash.delulu3i4@gmail.com>
2026-02-12 22:58:27 -0500

    addig github discord integratoin

--- .os/cli.py
+++ .os/cli.py
@@ -1,5 +1,20 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "typer>=0.23.0",
+# ]
+# ///
+
 # TODO: script will add unbinding items automatically
 # TODO: ensure when create file its has comment about how
 # not to edit directly but auto generated
 # TODO: have installed via uvx with bashrc alias config
 # TOOD: auto ad ~./bashrc line if doesnt already exist
+# TODO: cli.sh to install the uvx cli.py if not already, and pass through to uvx tool
+# TODO: publish which will add all changes in .os and push (see if auto run on succesful os sync)
+# TODO: command "os sync", "os publish"
+# TODO: add ~/.bashrc line if doesnt exist
+
+from __future__ import annotations
+
+print("hello")
--- .os/readme.md
+++ .os/readme.md
@@ -1,3 +1,8 @@
 # OS
 
 This configures to my specific computer set up to ensure eaisly have same configuration between devices (eg laptop and desktop)
+
+
+```bash
+uv add --script .os/cli.py 'pkg'
+```
